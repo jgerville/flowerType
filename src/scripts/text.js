4a9324cea5a0a6b69cf26747246ddc07606a001f
Julian Erville <jerville@gmail.com>
2021-10-29 09:18:08 -0400

    Add basic Text methods

--- src/scripts/text.js
+++ src/scripts/text.js
@@ -1,6 +1,34 @@
 class Text {
   constructor(body) {
     this.body = body;
-    this.current_char = body[0];
+    this.length = this.body.length;
+    this.idx = 0;
+    this.currentChar = this.body[this.idx];
+  }
+
+  // invoke if user correctly types currentChar, unless 
+  nextChar() {
+    if (!this.isFinished()) {
+      this.idx ++;
+      this.updateCurrentChar();
+    }
+  }
+
+  // invoke if user presses backspace, unless idx is 0
+  prevChar() {
+    if (this.idx > 0) {
+      this.idx --;
+      this.updateCurrentChar();
+    }
+  }
+
+  // invoke whenever the idx changes
+  updateCurrentChar() {
+    this.currentChar = this.body[this.idx];
+  }
+
+  // if the idx reaches the length, the user has gotten to the end of the text
+  isFinished() {
+    this.idx === this.length
   }
 }
