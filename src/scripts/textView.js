8ffc79ebe7f7c482502f256c08ac3882dff41c5a
Julian Erville <jerville@gmail.com>
2021-10-29 17:01:46 -0400

    Refactor event listener in TextView

--- src/scripts/textView.js
+++ src/scripts/textView.js
@@ -1,12 +1,13 @@
 const TextContent = require("./textContent");
 
-
 class TextView {
   constructor(textContent) {
-    this.CATIPSUM = `Cat ipsum dolor sit amet, put butt in owner's face poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree floof tum, tickle bum, jellybean footies curly toes destroy couch as revenge. Catching very fast laser pointer grass smells good cat slap dog in face. Ooooh feather moving feather! cereal boxes make for five star accommodation . Cuddle no cuddle cuddle love scratch scratch kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the embarrassment slap owner's face at 5am until human fills food dish refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am but scratch so kitty kitty pussy cat doll. Intently stare at the same spot kick up litter for cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers this is the day . Pet my belly, you know you want to; seize the hand and shred it! knock over christmas tree. And sometimes switches in french and say 'miaou' just because well why not so you're just gonna scroll by without saying meowdy?`
+    // this.CATIPSUM = `Cat ipsum dolor sit amet, put butt in owner's face poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree floof tum, tickle bum, jellybean footies curly toes destroy couch as revenge. Catching very fast laser pointer grass smells good cat slap dog in face. Ooooh feather moving feather! cereal boxes make for five star accommodation . Cuddle no cuddle cuddle love scratch scratch kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the embarrassment slap owner's face at 5am until human fills food dish refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am but scratch so kitty kitty pussy cat doll. Intently stare at the same spot kick up litter for cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers this is the day . Pet my belly, you know you want to; seize the hand and shred it! knock over christmas tree. And sometimes switches in french and say 'miaou' just because well why not so you're just gonna scroll by without saying meowdy?`
+    this.SHORTCATIPSUM = `Cat ipsum dolor sit amet, put butt in owner's face`
     this.IGNOREDKEYS = ["Tab", "Backspace", "Delete", "Shift", "Escape", "Alt", "CapsLock", "Control", "Fn", "FnLock", "Meta", "NumLock", "ScrollLock", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "End", "Home", "PageDown", "PageUp"]
-    this.textContent = new TextContent(this.CATIPSUM);
-    this.num_wrong_keydowns = 0;
+    this.textContent = new TextContent(this.SHORTCATIPSUM);
+    this.numWrongKeydowns = 0;
+    this.numWordsTyped = 0;
     this.renderText();
     // this.displayedIdx = 0;
     this.addBindings();
@@ -20,28 +21,36 @@ class TextView {
   }
 
   addBindings() {
-    document.addEventListener('keydown', (e) => {
-      const key = e.key;
-      console.log(key);
-      if (this._ismatch(key)) {
-        this.correctChar();
-      } else if (this._isIgnoredKey(key)) {
-        console.log("Looks like an ignored key was pressed.")
-      } else {
-        this.wrongChar();
-      }
-    })
+    document.addEventListener('keydown', this.typingListener.bind(this));
+  }
+
+  // currently not being called anywhere
+  removeBindings() {
+    document.removeEventListener('keydown', this.typingListener.bind(this));
+  }
+
+  typingListener(e) {
+    const key = e.key;
+    console.log(key);
+    if (this._ismatch(key)) {
+      if (this._isSpace(key)) this._incrementWordsTyped();
+      this.correctChar();
+    } else if (this._isIgnoredKey(key)) {
+      console.log(`${key} will not be counted as incorrect because it's on the ignore list.`)
+    } else {
+      this.wrongChar();
+    }
   }
 
   correctChar() {
     this.textContent.nextChar();
-    this.nextDisplayedIdx();
+    // this.nextDisplayedIdx();
     console.log(`matched! next up is ${this.textContent.currentChar}`)
   }
 
   wrongChar() {
     // this.nextDisplayedIdx();
-    this.num_wrong_keydowns ++;
+    this.numWrongKeydowns ++;
     console.log(`Wrong entry. the next char is: ${this.textContent.currentChar}`)
   }
 
@@ -60,6 +69,14 @@ class TextView {
   // _isBackspace(key) {
   //   return key === "Backspace";
   // }
+
+  _incrementWordsTyped() {
+    this.numWordsTyped ++;
+  }
+
+  _isSpace(key) {
+    return key === " ";
+  }
 
   _ismatch(key) {
     return this.textContent.isMatch(key);
