a21b0998e7384b8ecfceb490843f1aa24c941f4e
Julian Erville <jerville@gmail.com>
2021-11-01 12:56:53 -0400

    Refactor TextView

--- src/scripts/textView.js
+++ src/scripts/textView.js
@@ -5,13 +5,11 @@ import Timer from "./timer";
 class TextView {
   constructor(textContent) {
     this.CATIPSUM = `Cat ipsum dolor sit amet, put butt in owner's face poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree floof tum, tickle bum, jellybean footies curly toes destroy couch as revenge. Catching very fast laser pointer grass smells good cat slap dog in face. Ooooh feather moving feather! cereal boxes make for five star accommodation . Cuddle no cuddle cuddle love scratch scratch kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the embarrassment slap owner's face at 5am until human fills food dish refuse to come home when humans are going to bed; stay out all night then yowl like i am dying at 4am but scratch so kitty kitty pussy cat doll. Intently stare at the same spot kick up litter for cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers this is the day . Pet my belly, you know you want to; seize the hand and shred it! knock over christmas tree. And sometimes switches in french and say 'miaou' just because well why not so you're just gonna scroll by without saying meowdy?`
-    this.SHORTCATIPSUM = `Cat ipsum dolor sit amet, put butt in owner's face`
     this.IGNOREDKEYS = ["Tab", "Backspace", "Delete", "Shift", "Escape", "Alt", "CapsLock", "Control", "Fn", "FnLock", "Meta", "NumLock", "ScrollLock", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "End", "Home", "PageDown", "PageUp"]
     this.textContent = new TextContent(this.CATIPSUM);
     
     this.timerContainer = document.querySelector('.timer-container');
     this.timer = new Timer(30, this.timerContainer);
-
     this.textStats = new TextStats(this.timer.initialTime)
 
     this.boundTypingListener = this._typingListener.bind(this);
@@ -55,23 +53,25 @@ class TextView {
   }
 
   _typingListener(e) {
+    // in order to stop space bar from scrolling
+    e.preventDefault();
     const key = e.key;
 
     if (this.timer.over) {
       this.removeBindings();
     } else {
       if (this._ismatch(key)) {
-        this.startTimer();
-        this.correctChar();
+        this._startTimer();
+        this._correctChar();
       } else if (this._isIgnoredKey(key)) {
         console.log(`${key} will not be counted as incorrect because it's on the ignore list.`)
       } else {
-        this.wrongChar(key);
+        this._wrongChar(key);
       }
     }
   }
 
-  correctChar() {
+  _correctChar() {
     const correctEle = this._getCurrentElement();
     correctEle.classList.remove('current-char', 'wrong-char', 'still-wrong-char', 'very-wrong-char')
     correctEle.classList.add('correct-char');
@@ -82,13 +82,13 @@ class TextView {
     // console.log(`matched! next up is ${this.textContent.currentChar}`)
   }
 
-  startTimer() {
+  _startTimer() {
     if (!this.timer.running) {
       this.timer.start();
     }
   }
 
-  wrongChar(key) {
+  _wrongChar(key) {
     // first, update data in TextStats
     this.textStats.logWrongChar(this.textContent.currentChar, key);
 
