b5ababfe859d61b6d31df3fc0eeccb688290b4b7
Julian Erville <jerville@gmail.com>
2021-11-04 12:55:38 -0400

    Fix .gitignore

---
+++ dist/node_process-node_stream_web.js
@@ -0,0 +1,111 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunktypeprax"] = self["webpackChunktypeprax"] || []).push([["node_process-node_stream_web"],{
+
+/***/ "?f2e4":
+/*!************************!*\
+  !*** buffer (ignored) ***!
+  \************************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?7f3c":
+/*!************************!*\
+  !*** crypto (ignored) ***!
+  \************************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?d9bc":
+/*!**********************!*\
+  !*** http (ignored) ***!
+  \**********************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?1be1":
+/*!***********************!*\
+  !*** https (ignored) ***!
+  \***********************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?2fd0":
+/*!************************!*\
+  !*** stream (ignored) ***!
+  \************************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?9427":
+/*!*********************!*\
+  !*** url (ignored) ***!
+  \*********************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?4016":
+/*!**********************!*\
+  !*** util (ignored) ***!
+  \**********************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?7609":
+/*!**********************!*\
+  !*** zlib (ignored) ***!
+  \**********************/
+/***/ (function() {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "node:process":
+/*!********************!*\
+  !*** node:process ***!
+  \********************/
+/***/ (function() {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:process\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at /Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:825:25\n    at Hook.eval [as callAsync] (eval at create (/Users/julian/Desktop/aA/typePrax/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/julian/Desktop/aA/typePrax/node_modules/tapable/lib/Hook.js:18:14)\n    at Object.processResource (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:822:8)\n    at processResource (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:171:10)\n    at runLoaders (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:397:2)\n    at NormalModule._doBuild (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:812:3)\n    at NormalModule.build (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:956:15)\n    at /Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/Compilation.js:1361:12");
+
+/***/ }),
+
+/***/ "node:stream/web":
+/*!***********************!*\
+  !*** node:stream/web ***!
+  \***********************/
+/***/ (function() {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:stream/web\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at /Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:825:25\n    at Hook.eval [as callAsync] (eval at create (/Users/julian/Desktop/aA/typePrax/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Object.processResource (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:822:8)\n    at processResource (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:171:10)\n    at runLoaders (/Users/julian/Desktop/aA/typePrax/node_modules/loader-runner/lib/LoaderRunner.js:397:2)\n    at NormalModule._doBuild (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:812:3)\n    at NormalModule.build (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:956:15)\n    at /Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/Compilation.js:1361:12\n    at NormalModule.needBuild (/Users/julian/Desktop/aA/typePrax/node_modules/webpack/lib/NormalModule.js:1243:32)");
+
+/***/ })
+
+}]);
--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ const config = {
     }
   },
   plugins: [new MiniCssExtractPlugin()],
+  // devtool: 'source-map',
 };
 
 module.exports = (env, argv) => {
