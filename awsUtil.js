c3f2830e9b6d7b807a48041ac8614246e5128a98
Julian Erville <jerville@gmail.com>
2023-04-07 19:51:26 -0400

    Clean up awsUtil file.

--- awsUtil.js
+++ awsUtil.js
@@ -5,40 +5,9 @@ AWS.config.update({
   endpoint: "https://dynamodb.us-east-2.amazonaws.com"
   // endpoint: "http://localhost:8000"
 });
-
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-// CREATING A TABLE
-// 
-// const dynamodb = new AWS.DynamoDB();
 
-// var params = {
-//   TableName : "Scores",
-//   KeySchema: [     
-//     { AttributeName: "username", KeyType: "HASH"},  //Partition key
-//     { AttributeName: "wpm", KeyType: "RANGE" }  //Sort key
-//   ],
-//   AttributeDefinitions: [     
-//     { AttributeName: "username", AttributeType: "S" },
-//     { AttributeName: "wpm", AttributeType: "N" }
-//   ],
-//   ProvisionedThroughput: {     
-//     ReadCapacityUnits: 25, 
-//     WriteCapacityUnits: 25
-//   }
-// };
-
-// dynamodb.createTable(params, function(err, data) {
-//   if (err) {
-//     console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-//   } else {
-//     console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-//   }
-// });
-
-
-// ADDING TO TABLE
-// 
 // example input: postScore("normal", "John", 81, 0);
 async function postScore(kind, name, wpm, errors) {
   try {
@@ -59,29 +28,6 @@ async function postScore(kind, name, wpm, errors) {
     console.log(err);
   }
 }
-
-// SCANNING A TABLE
-// 
-// returns: an object; the useful data array is in key "Items"
-// async function fetchScores(kind) {
-//   let scores;
-//   try {
-//     const params = {
-//       TableName : "Scores",
-//       KeyConditionExpression: "kind = :kind",
-//       ExpressionAttributeValues: {
-//         ":kind": kind
-//       },
-//       ScanIndexForward: false
-//     };
-//     const result = await docClient.query(params).promise();
-//     scores = result;
-//     // to get the array: result.Items
-//   } catch (err) {
-//     console.log(err);
-//   }
-//   return scores;
-// }
 
 // returns: the array itself
 async function fetchScores(kind) {
